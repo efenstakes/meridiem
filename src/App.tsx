f0533b29c3fa60e7af3bcb775840984edcbf89b4
Efen Stakes <efenstakes101@gmail.com>
2021-05-14 21:58:24 +0300

    make app installable and update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
+
+
+// page
+import HomePage from './pages/home.page';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -26,11 +29,8 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
         <Route exact path="/">
-          <Redirect to="/home" />
+          <HomePage />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
